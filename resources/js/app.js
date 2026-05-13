e73434a622c951f8e07b40d2fefe3fb2784138ce
Anton <777elisar777@gmail.com>
2026-05-13 12:42:35 +0200

    Add first 2 components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
-import './bootstrap';
+// импортируем функцию создания Vue-приложения
+import { createApp } from 'vue';
+
+// импортируем наш компонент
+import HelloWorld from './components/HelloWorld.vue';
+
+/*
+    createApp() создает Vue-приложение.
+
+    То есть запускает Vue.
+*/
+createApp(HelloWorld).mount('#app');
+
+/*
+    mount('#app')
+
+    Говорит Vue:
+
+    "Найди HTML-элемент с id='app'
+    и управляй им"
+*/
+
---
+++ resources/js/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+    import MyFirstComponent from './MyFirstComponent.vue';
+    // обычная переменная JavaScript
+    const name = "SomeOne";
+
+    /*
+    Пока это НЕ реактивная переменная.
+    Просто обычная JS-переменная.
+    */
+</script>
+
+<template>
+    <!--
+        Vue позволяет вставлять JS-данные
+        внутрь HTML через {{ }}
+    -->
+
+    <h1>Привет, {{ name }}</h1>
+
+    <MyFirstComponent />
+</template>
