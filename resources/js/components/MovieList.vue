f8be9bd6b9d399fc361997e6e2d7457881798bcd
Anton <777elisar777@gmail.com>
2026-05-15 21:01:10 +0200

    Do 7th lesson

--- resources/js/components/MovieList.vue
+++ resources/js/components/MovieList.vue
@@ -8,20 +8,42 @@
             title: 'Matrix',
             year: '1999',
             watched: false,
+            raiting: 12,
         },
         {
             id: 2,
             title: 'Mask',
             year: '1994',
             watched: false,
+            raiting: 3,
         },
         {
             id:3,
             title: 'Avatar',
             year: '2015',
             watched: true,
+            raiting: 6,
         },
     ]);
+
+    function delFilm(filmID){
+        films.value = films.value.filter(film => {
+            return film.id !== filmID;   
+        })
+    }
+
+    function changeRating(filmId, digit){
+        
+        //Находим нужный фильм из массива по id
+        const film = films.value.find(f => f.id === filmId);
+
+        /* Если фильм найден и его рейтинг больше ноля */
+        if(film && film.raiting !== 0){
+            film.raiting = film.raiting + digit;
+        } if (film && film.raiting === 0) {
+            film.raiting = 1;
+        } 
+    }
 
 </script>
 <template>
@@ -31,12 +53,26 @@
             <li v-for="(movie, index) in films" :key="movie.id" >
                 <div class="card">
                     <h2>Красиво оформленная карточка фильма по заказу Цифровичка :))</h2>
+                    
                     <div class="cardBody">
                         
                         <p><b>{{ index + 1 }}. {{ movie.title }}: <i>{{ movie.year }}</i></b></p>
 
+                        <p v-show="movie.raiting > 8">🔥 Топ фильм</p>
+
                         <button @click="movie.watched = !movie.watched"><p><span v-if="movie.watched">✅ Просмотрен</span><span v-else>❌ Не просмотрен</span></p></button>
                     </div>
+
+                    <div  class="delButCont">
+                        <div class="delButCont">
+                            <button @click="changeRating(movie.id, 1)">+</button>
+                            <button @click="changeRating(movie.id, -1)">-</button>
+                            <p>Rating: {{ movie.raiting }}</p>
+                        </div>
+
+                        <button @click="delFilm(movie.id)">Delete film</button>
+                    </div>
+
                 </div>
             </li>
         </ol>
@@ -67,6 +103,18 @@
     .cardBody{
         display: flex;
         flex-direction: row;
-        justify-content: space-around;
+        justify-content: space-between;
+        margin-bottom: .5rem;
+    }
+    .delButCont{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .delButCont > button{
+        padding: .5rem;
+        border: 1px solid black;
+        border-radius: 20px;
+        background-color: aliceblue;
     }
 </style>
